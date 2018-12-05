471e35c11f7591afaf6eb5d6675c14f5ef5317f6
Kevin Cong Pei <kpsuperplane@fb.com>
2018-12-04 17:13:42 -0800

    Different powershell format

--- src/refmterr/runTests.cmd
+++ src/refmterr/runTests.cmd
@@ -1,9 +1,13 @@
-:<<BATCH
-    C:\>PowerShell
-BATCH
+: # This is a special script which intermixes both sh
+: # and cmd code. It is written this way because it is
+: # used in system() shell-outs directly in otherwise
+: # portable code. See https://stackoverflow.com/questions/17510688
+: # for details.
+
 cd src/refmterr
-refmt ./tests/test.re --print ml > ./tests/test.ml
-ocaml ./tests/test.ml
 
+:; refmt ./tests/test.re --print ml > ./tests/test.ml
+:; ocaml ./tests/test.ml
+:; exit
 
-
+powershell -Command 'refmt ./tests/test.re --print ml > ./tests/test.ml && ocaml ./tests/test.ml'
