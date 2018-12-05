98fab88891a217eaa4ce933500d4d954877b63cc
Kevin Cong Pei <kpsuperplane@fb.com>
2018-12-05 13:26:31 -0800

    ls all the things

--- src/refmterr/runTests.cmd
+++ src/refmterr/runTests.cmd
@@ -1,5 +1,9 @@
 cd src/refmterr
 refmt ./tests/test.re --print ml > ./tests/test.ml
 ls ../../_esy/default
+ls ../../_esy/default/build
+ls ../../_esy/default/build/install
+ls ../../_esy/default/build/install/default
+ls ../../_esy/default/build/install/default/bin
 npm root -g
 ocaml ./tests/test.ml
