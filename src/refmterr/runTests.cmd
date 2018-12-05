78807f9501e5b0542dc9c83ba13bbef00880d6dc
Kevin Cong Pei <kpsuperplane@fb.com>
2018-12-05 10:56:32 -0800

    Echo

--- src/refmterr/runTests.cmd
+++ src/refmterr/runTests.cmd
@@ -1,4 +1,5 @@
 cd src/refmterr
 refmt ./tests/test.re --print ml > ./tests/test.ml
-ls ../../_esy/default/build/install
+ls ../../
+echo "$(npm config get prefix)/lib/node_modules"
 ocaml ./tests/test.ml
