e8329bd16f8ea8d054c62ea915fe4eac1eda279a
Kevin Cong Pei <kpsuperplane@fb.com>
2018-12-04 17:37:30 -0800

    Use esy x

--- src/refmterr/runTests.cmd
+++ src/refmterr/runTests.cmd
@@ -1,13 +1,3 @@
-: # This is a special script which intermixes both sh
-: # and cmd code. It is written this way because it is
-: # used in system() shell-outs directly in otherwise
-: # portable code. See https://stackoverflow.com/questions/17510688
-: # for details.
-
 cd src/refmterr
 refmt ./tests/test.re --print ml > ./tests/test.ml
-
-:; ocaml ./tests/test.ml
-:; exit
-
-Powershell -Command 'ocaml ./tests/test.ml'
+ocaml ./tests/test.ml
