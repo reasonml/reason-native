04325784e1add4b70656e43cb846f55a7343e869
Kevin Cong Pei <kpsuperplane@fb.com>
2018-12-13 20:43:50 -0800

    Add prepublish script

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for pkg in pastel-console pastel console refmterr rely; do
-    esy "@${pkg}" "$@"
+    "$@" "${pkg}"
 done
---
+++ scripts/test-prepublish.sh
@@ -0,0 +1,2 @@
+node ./scripts/esy-prepublish.js $1.json
+rm -rf ./_release
