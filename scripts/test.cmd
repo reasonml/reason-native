b61f1c849ecdb1907bdbecfd27626dd56314479d
Kyle Davis <kad@fb.com>
2018-12-17 15:27:33 -0800

    Add rely tests to esy test command (#57)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/reason-native/pull/57
    
    Trying to get the `esy test` command to run all the Rely tests we just set up. Let's see how this works in CI
    
    Reviewed By: bandersongit
    
    Differential Revision: D13476611
    
    fbshipit-source-id: e2e8411cf5ee1ae3b3a4eb9b021dcce2017cadca

---
+++ scripts/test.cmd
@@ -0,0 +1,10 @@
+:; set -e
+
+# This will be the location of this script.
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Run refmterr tests.
+esy x bash -cx ./src/refmterr/runTests.cmd
+
+# Run ReasonNativeTests.exe with correct root set.
+REASON_NATIVE_ROOT="$DIR/../" esy x "ReasonNativeTests.exe" "$@"
